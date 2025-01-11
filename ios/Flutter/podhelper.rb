ff1ca931a01d56c2779f38dc1c1357c258e11a61
Your Name <you@example.com>
2025-01-11 21:35:37 +0530

    .

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -1,18 +1,24 @@
-unless File.exist?(File.expand_path('ios/Flutter/flutter_export_environment.sh', __dir__))
+# unless File.exist?(File.expand_path('ios/Flutter/flutter_export_environment.sh', __dir__))
+#     raise 'The Flutter podhelper script requires the flutter_export_environment.sh script. Make sure your Flutter project is properly configured.'
+#   end
+  
+
+#   require File.expand_path('ios/Flutter/flutter_export_environment.sh', __dir__)
+  
+#   def flutter_install_all_ios_pods(flutter_application_path)
+#     flutter_root = File.expand_path('..', flutter_application_path)
+#     flutter_ios_engine = File.expand_path('bin/cache/artifacts/engine/ios', flutter_root)
+#     pod 'Flutter', :path => File.join(flutter_application_path, '.symlinks', 'flutter', 'ios')
+  
+#     if File.exist?(File.join(flutter_application_path, '.symlinks', 'flutter', 'ios', 'plugin_pods.rb'))
+#       load File.join(flutter_application_path, '.symlinks', 'flutter', 'ios', 'plugin_pods.rb')
+#     end
+#   end
+  
+unless File.exist?(File.expand_path('../Flutter/flutter_export_environment.sh', __dir__))
     raise 'The Flutter podhelper script requires the flutter_export_environment.sh script. Make sure your Flutter project is properly configured.'
   end
   
-
-  require File.expand_path('ios/Flutter/flutter_export_environment.sh', __dir__)
-  
-  def flutter_install_all_ios_pods(flutter_application_path)
-    flutter_root = File.expand_path('..', flutter_application_path)
-    flutter_ios_engine = File.expand_path('bin/cache/artifacts/engine/ios', flutter_root)
-    pod 'Flutter', :path => File.join(flutter_application_path, '.symlinks', 'flutter', 'ios')
-  
-    if File.exist?(File.join(flutter_application_path, '.symlinks', 'flutter', 'ios', 'plugin_pods.rb'))
-      load File.join(flutter_application_path, '.symlinks', 'flutter', 'ios', 'plugin_pods.rb')
-    end
-  end
+  require File.expand_path('../Flutter/flutter_export_environment.sh', __dir__)
   
   
