73b269a8e52613aea3040c0d7ae9e56a7f31afb3
Your Name <you@example.com>
2025-01-11 10:35:04 +0530

    .

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -1,4 +1,13 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'podhelper.rb'))
-
-
-
+# podhelper.rb file for integrating Flutter dependencies into iOS via CocoaPods
+unless File.exist?(File.expand_path('../flutter_export_environment.sh', __dir__))
+    raise 'The Flutter podhelper script requires the flutter_export_environment.sh script. ' \
+          'Make sure your Flutter project is properly configured.'
+  end
+  
+  require File.expand_path('../flutter_export_environment.sh', __dir__)
+  
+  def flutter_install_all_ios_pods(flutter_application_path)
+    flutter_root = File.expand_path('..', flutter_application_path)
+    load File.join(flutter_root, '.ios', 'Flutter', 'podhelper.rb')
+  end
+  
