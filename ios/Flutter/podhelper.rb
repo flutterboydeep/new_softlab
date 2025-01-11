a7b3fe12f261f325218dcf1ead00d7db71b2db40
Your Name <you@example.com>
2025-01-11 14:44:02 +0530

    .

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -3,11 +3,17 @@ unless File.exist?(File.expand_path('../flutter_export_environment.sh', __dir__)
     raise 'The Flutter podhelper script requires the flutter_export_environment.sh script. Make sure your Flutter project is properly configured.'
   end
   
+  # Load Flutter environment variables
   require File.expand_path('../flutter_export_environment.sh', __dir__)
   
   def flutter_install_all_ios_pods(flutter_application_path)
     flutter_root = File.expand_path('..', flutter_application_path)
-    load File.join(flutter_root, 'ios', 'Flutter', 'podhelper.rb')
-  end
+    flutter_ios_engine = File.expand_path('bin/cache/artifacts/engine/ios', flutter_root)
+    pod 'Flutter', :path => File.join(flutter_application_path, '.symlinks', 'flutter', 'ios')
   
+    # Add more Flutter dependencies if necessary
+    if File.exist?(File.join(flutter_application_path, '.symlinks', 'flutter', 'ios', 'plugin_pods.rb'))
+      load File.join(flutter_application_path, '.symlinks', 'flutter', 'ios', 'plugin_pods.rb')
+    end
+  end
   
